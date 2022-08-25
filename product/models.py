0b3afba3bfad5d913a086609711c9788ee056a15
Daria Zhuravlova <104871252+daria-niklaevna@users.noreply.github.com>
2022-08-25 11:44:07 +0300

    Update models.py

--- product/models.py
+++ product/models.py
@@ -3,30 +3,31 @@ import uuid
 
 from django.conf import settings
 from django.db import models
-from django.utils.text import slugify
 
 
 def product_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.name)}-{uuid.uuid4()}.{extension}"
+    filename = f"{uuid.uuid4()}.{extension}"
 
     return os.path.join("uploads/products/", filename)
-
-
-class ImageProduct(models.Model):
-    title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to=product_image_file_path)
-
-    def __str__(self):
-        return str(self.name)
 
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ManyToManyField(ImageProduct, blank=True, related_name='images')
     creation_date = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+    def save(
+        self, *args, **kwargs
+    ):
+        super(Product, self).save(*args, **kwargs)
+
+
+class ImageProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to=product_image_file_path)
+
