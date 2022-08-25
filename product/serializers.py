fd02c85ee9b3412d4d8f10a86b667d17ad1f8720
Daria Zhuravlova <104871252+daria-niklaevna@users.noreply.github.com>
2022-08-25 11:45:17 +0300

    Update serializers.py

--- product/serializers.py
+++ product/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from product.models import Product, ImageProduct
@@ -15,16 +16,24 @@ class ImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ImageProduct
-        fields = ('id', 'title', 'image')
+        fields = ('image',)
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
+    images = ImageSerializer(many=True, read_only=False, allow_empty=False)
+    author = UserSerializer(many=False, read_only=True)
     class Meta:
         model = Product
-        fields = ('id', 'title', 'price', 'author', 'image', 'creation_date')
+        fields = ('id', 'title', 'price', 'author', 'images', 'creation_date')
 
+    def create(self, validated_data):
+        with transaction.atomic():
+            images = validated_data.pop("images")
+            product = Product.objects.create(**validated_data)
+            for image in images:
+                ImageProduct.objects.create(product=product, **image)
+            return product
 
 class ProductListSerializer(ProductSerializer):
-    image = ImageSerializer(many=True, read_only=True)
-    author = UserSerializer(many=False, read_only=True)
+    images = ImageSerializer(many=True, read_only=True)
+
